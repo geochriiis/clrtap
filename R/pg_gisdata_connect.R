6edeefca4ec8e2cbed98c93a43276956d1545b7d
Christopher Evangelides <christopher.evangelides@ricardo.com>
2024-07-12 13:28:20 +0100

    Cleans branch

--- R/pg_gisdata_connect.R
+++
@@ -1,9 +0,0 @@
-#Connect to NAEI database
-conn <- dbConnect(RPostgres::Postgres(), 
-    dbname = Sys.getenv("dbname_gisdata"), 
-    host = Sys.getenv("dbhost_gisdata1"), 
-    port = Sys.getenv("dbport_gisdata1"), 
-    user = Sys.getenv("dbuser_gisdata1"), 
-    password = Sys.getenv("dbpw_gisdata1")
-)
-
